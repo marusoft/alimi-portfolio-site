f77d65747fc01f3f5931a3776b5467b4e3d787df
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2023-04-05 11:55:21 +0100

    re-arranged code

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@ const hambugerContain = document.querySelector('#hambuger-contain');
 const navList = document.querySelector('.nav-list');
 const presentYear = document.getElementById('getYear');
 
+const today = new Date();
+const year = today.getFullYear();
+presentYear.innerHTML = `&copy; ${year} Marusoft`
+
+
 const toggleBar = () => {
   hambugerContain.classList.toggle('toggler');
   navList.classList.toggle('toggler');
@@ -11,6 +16,3 @@ hambugerContain.addEventListener('click', toggleBar);
 navList.addEventListener('click', toggleBar);
 
 
-const today = new Date();
-const year = today.getFullYear();
-presentYear.innerHTML = `&copy; ${year} Marusoft`
