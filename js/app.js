a6f8310a251c0091d795b25fcc0163fbe780fe6c
Alimi Kehinde Morufudeen <alimi.kmaruf@gmail.com>
2023-04-05 11:46:20 +0100

    fix broken project links and make year dynamically

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 const hambugerContain = document.querySelector('#hambuger-contain');
 const navList = document.querySelector('.nav-list');
+const presentYear = document.getElementById('getYear');
 
 const toggleBar = () => {
   hambugerContain.classList.toggle('toggler');
@@ -8,3 +9,8 @@ const toggleBar = () => {
 
 hambugerContain.addEventListener('click', toggleBar);
 navList.addEventListener('click', toggleBar);
+
+
+const today = new Date();
+const year = today.getFullYear();
+presentYear.innerHTML = `&copy; ${year} Marusoft`
